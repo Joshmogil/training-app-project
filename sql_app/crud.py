6f8fa30d1613b0c0699baad465f4bd5fc62c1865
Josh Mogil <jmogil198@gmail.com>
2021-12-29 00:07:04 -0500

    First commit

---
+++ sql_app/crud.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def get_password_hash_by_email(db: Session, email: str):
+    db_user = db.query(models.User).filter(models.User.email == email).first()
+    return db_user.hashed_password
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: schemas.UserCreate, hashedPassword):
+    
+    db_user = models.User(email=user.email, hashed_password=hashedPassword)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
