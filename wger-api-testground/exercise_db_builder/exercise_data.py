b0f131fc13fb8e517b3dc687566b0f0008827b3a
Josh Mogil <jmogil198@gmail.com>
2022-01-04 19:47:26 -0500

    Cleaned the exercise data

--- wger-api-testground/exercise_db_builder/exercise_data.py
+++ wger-api-testground/exercise_db_builder/exercise_data.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
+import json
 
 @dataclass
 class Exercise():
+
+    app_id: int    
+    regularity: int
+
     wg_id: int
     name: str
     category: str
@@ -9,5 +14,12 @@ class Exercise():
 
     muscles: list
     variation_fam_members: list
+
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+
+
 
 
