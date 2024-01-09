3f3a57484b8b26345d8e155016edc2f1df0bd4d8
Josh Mogil <jmogil@pop-os.localdomain>
2024-01-09 18:24:26 -0500

    firestore document parent class commit, init, get functions

--- firestore/user/firestore_utils.py
+++ firestore/user/firestore_utils.py
@@ -1,5 +1,19 @@
+import dataclasses
 import os
 from google.cloud import firestore
+import pydantic
+
+
+"""
+    doc_ref = db.collection("users").document("alovelace")
+    doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+
+    doc_ref = db.collection("users").document("aturing")
+    doc_ref.set({"first": "Alan", "middle": "Mathison", "last": "Turing", "born": 1912})
+
+    users_ref = db.collection("users")
+    docs = users_ref.stream()
+"""
 
 
 class FirestoreDocument:
@@ -7,28 +21,54 @@ class FirestoreDocument:
             self,
             firestore_client: firestore.Client,
             user_id: str,
-            collection_key: str
+            collection_key: str,
+            pydantic_model: pydantic.BaseModel
             ):
-        self.db=firestore_client
-        self.collection=collection_key 
-        self.document_id = collection_key.to_lower() + "_" + user_id 
+        self.__db=firestore_client
+        self.__collection=collection_key 
+        self.__document_id=collection_key.lower() + "_" + user_id 
+        self.__model=pydantic_model
+        #self.__data: pydantic.BaseModel =None
+        #self.__data= self.__load_document_into_model()
     
-    def __load_document_into_model(self):
+    def init_data_model(self, data_model: pydantic.BaseModel) -> bool:
+        self.__commit_model_to_firestore(data_model)
+        return True
+
+
+    def commit_model(self, data_model: pydantic.BaseModel) -> bool:#, data_model: pydantic.BaseModel) -> bool:
+        self.__commit_model_to_firestore(data_model)
+        return True
+        
+    def get_data_model(self):
+        data_model=self.__load_document_into_model()
+        return data_model
+
+    
+    
+    def __load_document_into_model(self) -> pydantic.BaseModel or None:
         """Loads json document from firestore into pydantic model."""
-        pass
-
-
-    def __dump_model_into_document(self):
-        """Dumps pydantic model into json document."""
-        pass
-
+        try:
+            doc_ref = self.__db.collection(self.__collection).document(self.__document_id).get()
+            if doc_ref.exists:
+                doc_data = doc_ref.to_dict()
+                return self.__model(**doc_data)
+            else:
+                "Cannot load empty document."
+        except:
+            return None
+        
     
-    def commit_document(self):
+    def __commit_model_to_firestore(self, data_model: pydantic.BaseModel):
         """Commits modified document to firestore db."""
-        pass
+        #print(data_model)
+        doc_ref = self.__db.collection(self.__collection).document(self.__document_id)
+        doc_ref.set(data_model.dict())
+    
 
 
 
 def use_local_firestore():
     FIRESTORE_EMULATOR_HOST='127.0.0.1:8083'
     os.environ['FIRESTORE_EMULATOR_HOST']=FIRESTORE_EMULATOR_HOST
+
--- firestore/user/journal.py
+++ firestore/user/journal.py
@@ -4,13 +4,58 @@ from firestore_utils import FirestoreDocument, use_local_firestore
 
 COLLECTION_KEY: str ="user_journals"
 
+
+
+from enum import Enum
+from typing import Union
+from typing_extensions import Annotated
+import pydantic
+from pydantic.config import ConfigDict
+from pydantic import BaseModel, Field
+
+class FooBar(BaseModel):
+    count: int
+    size: Union[float, None] = None
+
+
+class Gender(str, Enum):
+    male = 'male'
+    female = 'female'
+    other = 'other'
+    not_given = 'not_given'
+
+
+class MainModel(BaseModel):
+    """
+    This is the description of the main model
+    """
+
+    model_config = ConfigDict(title='Main')
+
+    foo_bar: FooBar
+    gender: Annotated[Union[Gender, None], Field(alias='Gender')] = None
+    snap: int = Field(
+        42,
+        title='The Snap',
+        description='this is the value of snap',
+        gt=30,
+        lt=50,
+    )
+
+
 class UserJournal(FirestoreDocument):
     def __init__(
             self,
             firestore_client: firestore.Client,
             user_id: str
             ):
-        super().__init__(firestore_client, user_id=user_id, collection_key=COLLECTION_KEY)
+        super().__init__(
+            firestore_client=firestore_client,
+            user_id=user_id,
+            collection_key=COLLECTION_KEY,
+            pydantic_model=MainModel
+            )
+        
     
     
 
@@ -20,20 +65,30 @@ if __name__ == "__main__":
     use_local_firestore()
     
     new_journal = UserJournal(
-                              firestore_client=firestore.Client(project="my-project-id"),
-                              user_id="jmogil123"
-                              )
+        firestore_client=firestore.Client(project="my-project-id"),
+        user_id="jmogil123"
+        )
+    
+    d0 ={
+        "model_config": {"title": "Main"},
+        "foo_bar": {"count": 10, "size": 5.5},
+        "gender": "male",
+        "snap": 35
+    }
+
+    
+
+    new_journal.init_data_model(data_model=MainModel(**d0))
+    journal_data: MainModel = new_journal.get_data_model()
+    print(journal_data)
+
+    journal_data.model_config["title"] ="Two Brothas"
+    new_journal.commit_model(journal_data)
+    journal_data: MainModel = new_journal.get_data_model()
+    print(journal_data)
 
 
+    
 
-    doc_ref = db.collection("users").document("alovelace")
-    doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
-
-    doc_ref = db.collection("users").document("aturing")
-    doc_ref.set({"first": "Alan", "middle": "Mathison", "last": "Turing", "born": 1912})
-
-    users_ref = db.collection("users")
-    docs = users_ref.stream()
-
-    for doc in docs:
-        print(f"{doc.id} => {doc.to_dict()}")
+    #for doc in docs:
+    #    print(f"{doc.id} => {doc.to_dict()}")
